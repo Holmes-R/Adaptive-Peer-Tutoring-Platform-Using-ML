026759916b3385747fe0b37b032c0d0cda413c66
Holmes-R <hariharanrameshbabu2004@gmail.com>
2025-04-05 22:27:17 +0530

    feat ( frontend ) : Testing

--- Adaptive_Learning/settings.py
+++ Adaptive_Learning/settings.py
@@ -4,7 +4,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
